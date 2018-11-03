8f899083d43debca6250dc2079ad393dd7e5d168
Paula Muldoon <pmuldoon@umich.edu>
2018-11-03 21:59:13 +0000

    extract getParams function and write unit test

--- src/cocoaJS.js
+++ src/cocoaJS.js
@@ -11,14 +11,19 @@ class CocoaJS {
         Object.entries(scenarios).forEach((scenario) => {
             it(setup.it, () => {
                 const params = [];
-                Object.entries(scenario[1].params).forEach((scenarioParam) => {
-                    params.push(scenarioParam[1]);
-                });
+                CocoaJS.getParams(scenario, params);
                 const actual = codeFile[setup.methodName](...params);
                 expect(actual, `${JSON.stringify(scenario[1].message)} ${JSON.stringify(scenario[1].params)}`)
                     .to.eq(scenario[1].expected);
             });
         });
+    }
+
+    static getParams(scenario, params) {
+        Object.entries(scenario[1].params).forEach((scenarioParam) => {
+            params.push(scenarioParam[1]);
+        });
+        return params;
     }
 }
 module.exports = CocoaJS;
