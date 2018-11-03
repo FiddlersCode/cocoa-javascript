265102e3f58e6328c12b71e0ae256718e228609e
Paula Muldoon <pmuldoon@umich.edu>
2018-11-03 22:15:29 +0000

    add test for missing param

--- test/unit/cocoaJSTest.js
+++ test/unit/cocoaJSTest.js
@@ -16,14 +16,36 @@ describe('test eq method', () => {
                         i: 1,
                         n: 7,
                     },
-                    message: 'number of PRCs used',
-                    expected: 1,
                 },
             ];
             const params = [];
             const actual = CocoaJs.getParams(scenario, params);
             const expected = [1, 0, 1, 7];
             expect(actual).to.deep.eq(expected);
+        });
+
+        it('should return an empty array with no params', () => {
+            const scenario = [
+                'scenario1', {
+                    params: {
+                    },
+                },
+            ];
+            const params = [];
+            const actual = CocoaJs.getParams(scenario, params);
+            const expected = [];
+            expect(actual).to.deep.eq(expected);
+        });
+
+        it('should throw an error if params are missing', () => {
+            const scenario = [
+                'scenario1', {
+                    message: {
+                    },
+                },
+            ];
+            const params = [];
+            expect(() => CocoaJs.getParams(scenario, params)).to.throw('Cannot convert undefined or null to object');
         });
     });
 });
