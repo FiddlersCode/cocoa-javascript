bb58d59aac68e3faf852a9f618845aa082587761
Paula Muldoon <pmuldoon@umich.edu>
2018-11-17 16:25:11 +0100

    fix npm issues with eslint and nyc

--- test/acceptance/test/Calculator.js
+++ test/acceptance/test/Calculator.js
@@ -4,8 +4,6 @@ const add = require('./params/add');
 
 describe('test eq method', () => {
     describe('addition method', () => {
-        console.log(add);
-
         CocoaJS.eq(
             add.setup().paramsFilePath,
             add.setup(),
