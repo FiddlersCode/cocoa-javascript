026fb1e146c38d38ad6027971f9a168fdf722662
Paula Muldoon <pmuldoon@umich.edu>
2018-11-17 17:57:43 +0100

    remove all src and test

--- src/ErrorMessages.js
+++
@@ -1,9 +0,0 @@
-class ErrorMessages {
-    static scenarioErrors() {
-        return {
-            noScenarios: 'Scenarios are required.',
-        };
-    }
-}
-
-module.exports = ErrorMessages;
--- test/acceptance/src/Calculator.js
+++
@@ -1,7 +0,0 @@
-class Calculator {
-    static add(number1, number2) {
-        return number1 + number2;
-    }
-}
-
-module.exports = Calculator;
